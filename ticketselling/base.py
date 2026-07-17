657aaafb587e2dc14f1c97d34e4bbbe45ca180a2
cdat13 <congdat130404@gmail.com>
2026-07-17 22:41:45 +0700

    de

--- ticketselling/base.py
+++ ticketselling/base.py
@@ -3,17 +3,14 @@ from flask import Flask
 
 
 def create_app(**config):
-       app = Flask(__name__)
-    FlaskDynaconf(app)
-
-    print("=" * 50)
-    print("Current ENV:", app.config.current_env)
-    print("Database URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
-    print("=" * 50)
-
-    app.config.load_extensions("EXTENSIONS")
-    app.config.update(config)
+    app = Flask(__name__)
+    FlaskDynaconf(app)  # config managed by Dynaconf
+    app.config.load_extensions(
+        "EXTENSIONS"
+    )  # Load extensions from settings.toml
+    app.config.update(config)  # Override with passed config
     return app
+
 
 def create_app_wsgi():
     # workaround for Flask issue
